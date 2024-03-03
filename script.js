a1359edbad4fa4a34ed462476ade3285ce33d8ed
Humam Afif <Humamafif2002@gmail.com>
2024-03-03 14:32:54 +0700

    Menambahkan web pribadi

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const notifikasi = document.querySelector('.notifikasi');
+const tombolClose = document.querySelector('.notifikasi-close-button');
+
+tombolClose.addEventListener('click', function () {
+    notifikasi.style.display = 'none';
+})
